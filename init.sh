78e67367d5ace040b835e109f399d9710ace9c31
Diego Rodríguez <diego.rodriguez@broadcom.com>
2022-11-12 17:35:45 +0100

    c1

--- init.sh
+++ init.sh
@@ -60,3 +60,9 @@ zowe plugins install ops-for-zowe-cli.tgz
 zowe plugins install endevor-bridge-for-git-for-zowe-cli.tgz
 zowe plugins install mat-analyze-for-zowe-cli.tgz
 zowe plugins install om-spool-for-zowe-cli.tgz
+
+code --install-extension ms-vscode.remote-explorer-0.1.2022103109.vsix
+code --install-extension broadcomMFD.code4z-extension-pack-2.0.1.vsix
+code --install-extension broadcomMFD.lsp-for-rexx-0.0.18.vsix 
+code --install-extension broadcomMFD.data-editor-for-mainframe-1.0.0.vsix
+code --install-extension broadcomMFD.bridge-for-git-explorer-0.3.0.vsix
