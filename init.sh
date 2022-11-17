493dc82eba31f854458987c751a65b76e5f5e3f2
Diego Rodríguez <diego.rodriguez@broadcom.com>
2022-11-17 16:24:51 +0100

    c1

--- init.sh
+++ init.sh
@@ -62,7 +62,6 @@ npm install -g @zowe/cli@zowe-v2-lts
 zowe plugins install @zowe/cics-for-zowe-cli@zowe-v2-lts @zowe/db2-for-zowe-cli@zowe-v2-lts @zowe/ims-for-zowe-cli@zowe-v2-lts @zowe/mq-for-zowe-cli@zowe-v2-lts @zowe/zos-ftp-for-zowe-cli@zowe-v2-lts
 zowe plugins install @zowe/secure-credential-store-for-zowe-cli
 
-zowe plugins install ca7-for-zowe-cli.tgz
 zowe plugins install @broadcom/ca7-for-zowe-cli@zowe-v2-lts
 zowe plugins install @broadcom/endevor-for-zowe-cli@zowe-v2-lts
 zowe plugins install @broadcom/endevor-bridge-for-git-for-zowe-cli@zowe-v2-lts
@@ -74,13 +73,12 @@ zowe plugins install @broadcom/om-spool-for-zowe-cli@zowe-v2-lts
 zowe plugins install @broadcom/sysview-for-zowe-cli@zowe-v2-lts
 zowe plugins install @broadcom/caview-for-zowe-cli@zowe-v2-lts
 
-zowe plugins install microfocus-zmf-cli-plugin-1.0.3.tgz
+zowe plugins install microfocus-zmf-cli-plugin-2.0.2.tgz
 
-code --install-extension ms-vscode.remote-explorer-0.1.2022103109.vsix
-code --install-extension broadcomMFD.code4z-extension-pack-2.0.1.vsix
-code --install-extension broadcomMFD.lsp-for-rexx-0.0.18.vsix 
-code --install-extension broadcomMFD.data-editor-for-mainframe-1.0.0.vsix
-code --install-extension broadcomMFD.bridge-for-git-explorer-0.3.0.vsix
+code --install-extension broadcomMFD.code4z-extension-pack
+code --install-extension broadcomMFD.lsp-for-rexx
+code --install-extension broadcomMFD.data-editor-for-mainframe
+code --install-extension broadcomMFD.bridge-for-git-explorer
 
 cp ./zowe_configs/sr01/zowe.config.json /root/.zowe/
 cp ./zowe_configs/sr01/zowe.schema.json /root/.zowe/
